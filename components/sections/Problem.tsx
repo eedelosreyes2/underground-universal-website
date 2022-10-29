b377590bd68106f559d3d95ba74bcdd7a461ee58
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-10-28 21:04:32 -0700

    problem

---
+++ components/sections/Problem.tsx
@@ -0,0 +1,40 @@
+const Problem = () => {
+  return (
+    <div className="section">
+      <div className="inner-container">
+        <h3>The Problem With Music Today</h3>
+        <div className="lg:flex">
+          <div>
+            <h2>Artist Support</h2>
+            <p>
+              When you sign to a record label, you often lose creative control
+              of your music. This includes being granted time and freedom to
+              create new albums that are unique to your creative vision as an
+              artist.
+            </p>
+          </div>
+          <div>
+            <h2>
+              It's <span className="red-text">your</span> art
+            </h2>
+            <p>
+              The top record labels have almost complete control over an
+              artist's music and growth, especially those new to the industry
+              who typically only receive less than 17-20% of their royalties.
+            </p>
+          </div>
+          <div>
+            <h2>Collaboration</h2>
+            <p>
+              Oversaturation of so many types of artists in different places
+              around the world stalls the creation of any music at all. This
+              results in stagnation of many artist careers.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Problem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Landing from "../components/sections/Landing";
 import Mission from "../components/sections/Mission";
+import Problem from "../components/sections/Problem";
 
 export default function Home() {
   return (
     <div>
       <Landing />
       <Mission />
+      <Problem />
     </div>
   );
 }
