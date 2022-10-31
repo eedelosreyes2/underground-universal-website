40cb658dbb873184904fba1e52bf1d96c181b916
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-10-30 21:50:13 -0700

    button2

--- components/sections/Join.tsx
+++ components/sections/Join.tsx
@@ -6,15 +6,16 @@ const Join = () => {
           Join the Underground Universal{" "}
           <span className="red-text">Community</span>
         </h2>
-        <p className="max-w-[780px] mx-auto">
+        <p className="max-w-[780px] mx-auto mb-10">
           Underground Universal is founded by an underground hip-hop producer
-          who saw the need for a label that wouldn’t take advantage of creators
+          who saw the need for a label that wouldn't take advantage of creators
           like him. Our belief is because it is your music, your art, your
-          royalties should be yours too. Unlike other labels, we don’t take
-          what’s yours. Through our online platform we strive to serve your
+          royalties should be yours too. Unlike other labels, we don't take
+          what's yours. Through our online platform we strive to serve your
           creative needs by providing easy access to collaborators, engineers,
           studios, music distribution and promotion.
         </p>
+        <div className="button-2">Join</div>
       </div>
     </div>
   );
