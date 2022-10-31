415a288b77a7dbd96e9b5055e0de9ec3170e2278
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-10-30 17:57:30 -0700

    montserrat

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Join from "../components/sections/Join";
 import Landing from "../components/sections/Landing";
 import Mission from "../components/sections/Mission";
@@ -8,6 +9,14 @@ import Services from "../components/sections/Services";
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Underground Universal</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <Landing />
       <Mission />
       <Problem />
