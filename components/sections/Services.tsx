d5d1f290e78919c3eda270b7f4e9d9ce32fe5741
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-11-09 20:14:38 -0800

    services section

--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -3,6 +3,50 @@ const Services = () => {
     <div className="section col">
       <div className="inner-container">
         <h3>Get the support you need</h3>
+        <div className="flex flex-wrap gap-5 justify-center">
+          <div className="card">
+            <h2>Collaboration & Sharing</h2>
+            <p>
+              Find fellow underground artists, share your music, and create
+              together.
+            </p>
+          </div>
+          <div className="card">
+            <h2>Studio Booking</h2>
+            <p>
+              We book, you create. Choose your studio and receive hassle-free
+              booking while you work on your music.
+            </p>
+          </div>
+          <div className="card">
+            <h2>Mixing & Mastering</h2>
+            <p>
+              Get in-house, online, and in-person studio options available near
+              you.
+            </p>
+          </div>
+          <div className="card">
+            <h2>Publishing</h2>
+            <p>
+              Seamless publishing provided by Songtrust. Exclusive to premium
+              members.
+            </p>
+          </div>
+          <div className="card">
+            <h2>Distribution</h2>
+            <p>
+              Effortless music distribution made possible by TuneCore. Get
+              unlimited uploads to 150+ digital stores worldwide including Apple
+              Music and Spotify.
+            </p>
+          </div>
+          <div className="card">
+            <h2>
+              <span className="red-text">Artist</span> Promotion
+            </h2>
+            <p>Guaranteed exposure, audience growth, community, and more.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
