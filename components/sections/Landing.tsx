981582740426d86f5159309002755df64b69fcc6
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-11-10 20:14:10 -0800

    easier to read over landing img

--- components/sections/Landing.tsx
+++ components/sections/Landing.tsx
@@ -2,15 +2,15 @@ const Landing = () => {
   return (
     <div
       className="section col h-[620px] bg-no-repeat bg-cover bg-center flex items-center"
-      style={{ backgroundImage: "url(landing-img.jpg)" }}
+      style={{ backgroundImage: 'url(landing-img.jpg)' }}
     >
-      <div className="inner-container">
+      <div className="inner-container mb-8 sm:mb-0">
         <h1>
-          The Hip-Hop label that works for the{" "}
+          The Hip-Hop label that works for the{' '}
           <span className="red-text">artist</span>
         </h1>
-        <p className="p-big mb-20">
-          It’s your music, your art. We take 0% of your royalties.
+        <p className="p-big font-bold mb-20">
+          It's your music, your art. We take 0% of your royalties.
         </p>
         <div className="button-1">Learn more</div>
       </div>
