6db92aea7303496b13b9adec17147220073e0dcb
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-12-06 21:46:56 -0800

    favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,6 +32,7 @@ export default function Home() {
       <Head>
         <title>Underground Universal</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
           rel="stylesheet"
